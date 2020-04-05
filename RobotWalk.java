9f65e3b1c818c97b9c1c1affce752a2be396b865
YOOJEHUN <dbwpgns0525@gmail.com>
2020-04-05 14:46:17 +0900

    Initial sharing of project

---
+++ RobotWalk.java
@@ -0,0 +1,33 @@
+
+/**
+ * 여기에 RobotWalk 클래스 설명을 작성하십시오.
+ * 
+ * 2018315005 유제훈 2018315033 태영준
+ * 2020.04.05 
+ */
+public class RobotWalk
+{
+    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 바꾸십시오
+    private int x;
+
+    /**
+     * RobotWalk 클래스의 객체 생성자
+     */
+    public RobotWalk()
+    {
+        // 인스턴스 변수의 초기화
+        x = 0;
+    }
+
+    /**
+     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
+     * 
+     * @param  y   메소드의 예제 매개변수
+     * @return     x 더하기 y 
+     */
+    public int sampleMethod(int y)
+    {
+        // 여기에 코드를 작성하십시오.
+        return x + y;
+    }
+}
