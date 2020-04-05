d1e66519a52453dfd22e221bc293c65d938cb8e5
YOOJEHUN <dbwpgns0525@gmail.com>
2020-04-05 14:55:25 +0900

    RobotWalk update main method

--- RobotWalk.java
+++ RobotWalk.java
@@ -7,27 +7,19 @@
  */
 public class RobotWalk
 {
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 바꾸십시오
-    private int x;
-
-    /**
-     * RobotWalk 클래스의 객체 생성자
-     */
-    public RobotWalk()
-    {
-        // 인스턴스 변수의 초기화
-        x = 0;
+    public static void main(String[] args){
+        int Speed = 50;
+        String trafficlight = red, green, yellow;
+        
+        result = RobotWalk(red);
+        
+        System.out.println(result);
+    
+    
+    
     }
-
-    /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
-     * 
-     * @param  y   메소드의 예제 매개변수
-     * @return     x 더하기 y 
-     */
-    public int sampleMethod(int y)
-    {
-        // 여기에 코드를 작성하십시오.
-        return x + y;
+    
+    public static String RobotWalk(){
+        return 
     }
 }
