193364b5f8909b945a6e3b95c622283f91db37c7
태영준 <ytae1999@naver.com>
2020-04-10 12:37:01 +0900

    merge

--- Robot.java
+++ Robot.java
@@ -1,4 +1,3 @@
-
 /**
  * RobotWalk 메소드에 대한 코드
  * 
@@ -6,35 +5,18 @@
  * @version (2020.04.10) 
  */
 public class Robot
-<<<<<<< HEAD
 {   
     public static int RobotWalk(String trafficLight, int speed){                  
         trafficLight = "green"; // 초기화 
         trafficLight = "yellow"; // 초기화 
         trafficLight = "red"; // 초기화 
         
-=======
-{    
-    public static int RobotWalk(String trafficLight, int speed){
-        speed = 5; // speed 값 5로 설정
->>>>>>> 2024b459e825bbf6031de483a0cb6f95face24b2
         if(trafficLight == "green"){    
-<<<<<<< HEAD
             if(trafficLight == "yellow"){
                 speed = (int)(speed*1.2);                 
             }else{
                 speed = 0;   
             }
-=======
-        
-      
-        }
-        else if(trafficLight == "yellow"){
-            speed = (int)(speed*1.2); 
-        }
-        else{
-            speed = 0;
->>>>>>> 2024b459e825bbf6031de483a0cb6f95face24b2
         }
         return speed;
     }
