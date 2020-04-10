62f8f5ffe5faa4f80ddf19d600220b133f01d0eb
jehunyoo <dbwpgns0525@gmail.com>
2020-04-10 12:38:05 +0900

    Merge

--- RobotTest.java
+++ RobotTest.java
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-.
-=======
 
->>>>>>> 8b2316d5b5ef09fcae4715e3de8dccde0d4a2e32
->>>>>>> 2024b459e825bbf6031de483a0cb6f95face24b2
->>>>>>> 535065296f2801e5a6f9d3ecbb7f80a64de48c42
 /**
  * Robot클래스를 실행시키는 메인 클래스
  *
@@ -17,18 +7,14 @@
  */
 public class RobotTest extends Robot
 {
-    public static void main(String[] args){        
-       
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         System.out.println("교통신호가 녹색이므로 " +  RobotWalk("green") + 
+
+        System.out.println("교통신호가 녹색이므로 " + RobotWalk("green", 5) +
+
 =======
         System.out.println("교통신호가 녹색이므로 " + RobotWalk("green", 5) +
->>>>>>> 8b2316d5b5ef09fcae4715e3de8dccde0d4a2e32
-=======
-        System.out.println("교통신호가 녹색이므로 " + RobotWalk("green", 5) +
->>>>>>> 8b2316d5b5ef09fcae4715e3de8dccde0d4a2e32
+
 =======
         System.out.println("교통신호가 녹색이므로 " + RobotWalk("green", 5) +
 >>>>>>> 8b2316d5b5ef09fcae4715e3de8dccde0d4a2e32
