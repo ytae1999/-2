2b9731c0d69f6ae61feb54e717a5da2e5a9fe009
태영준 <ytae1999@naver.com>
2020-04-10 11:17:29 +0900

    RobotTest 클래스 생성과 수정

---
+++ Robot.java
@@ -0,0 +1,23 @@
+
+/**
+ * RobotWalk 메소드에 대한 코드
+ * 
+ * @author (2018315005 유제훈, 2018315033 태영준)
+ * @version (2020.04.10) 
+ */
+public class Robot
+{    
+    public static int RobotWalk(String trafficLight){
+        int speed = 5; // speed 값 5로 설정
+        if(trafficLight == "green"){    
+            
+        }
+        else if(trafficLight == "yellow"){
+            speed = (int)(speed*1.2); 
+        }
+        else{
+            speed = 0;
+        }
+        return speed;
+    }
+}
--- RobotTest.java
+++ RobotTest.java
@@ -1,11 +1,11 @@
 
 /**
- * RobotWalk 메소드에 대한 코드
- * 
- * 2018315005 유제훈, 2018315033 태영준
- * 2020.04.05 
+ * Robot클래스를 실행시키는 메인 클래스
+ *
+ * @author (2018315005 유제훈, 2018315033 태영준)
+ * @version (2020.04.10)
  */
-public class RobotWalkSpeed
+public class RobotTest extends Robot
 {
     public static void main(String[] args){
         String trafficLight; // trafficLight 선언 
@@ -20,19 +20,5 @@ public class RobotWalkSpeed
                             "km/h" + " 속도로 보행 계속");
         System.out.println("교통신호가 빨간색이므로 " + RobotWalk("red") + 
                             "km/h" + " 속도로 보행 정지");
-    }
-    
-    public static int RobotWalk(String trafficLight){
-        int speed = 5; // speed 값 5로 설정
-        if(trafficLight == "green"){    
-            
-        }
-        else if(trafficLight == "yellow"){
-            speed = (int)(speed*1.2); 
-        }
-        else{
-            speed = 0;
-        }
-        return speed;
     }
 }
