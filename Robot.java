dc0925b81bec1f0320732a1911d38e23a5d60265
jehunyoo <dbwpgns0525@gmail.com>
2020-04-12 14:49:29 +0900

    Update Robot, RobotTest class

--- Robot.java
+++ Robot.java
@@ -1,4 +1,3 @@
-
 /**
  * RobotWalk 메소드에 대한 코드
  * 
@@ -11,22 +10,13 @@ public class Robot
     int speed; //변수 정의
 
     public static int RobotWalk(String trafficLight, int speed){                       
-        if(trafficLight != "green"){                    
+        if(trafficLight != "green"){            
             if(trafficLight == "yellow"){ 
-<<<<<<< HEAD
-                speed = (int)(speed*1.2); 
-                if(trafficLight == "red"){
-                    speed = 0;                      
-                }
-                
-            }   
-=======
                 speed = (int)(speed*1.2);                            
             }
             if(trafficLight == "red"){
                 speed = 0;                      
                 }     
->>>>>>> 6721e5165d6473f6082bc0e4903f08505218c18a
         }         
         return speed;
     }
