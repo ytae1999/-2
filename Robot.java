d69fc6e77ecb88e9a8cea315022e95827f9fc724
jehunyoo <dbwpgns0525@gmail.com>
2020-04-12 14:47:20 +0900

    update

--- Robot.java
+++ Robot.java
@@ -18,7 +18,7 @@ public class Robot
                     speed = 0;                      
                 }
                 
-            }
+            }   
         }         
         return speed;
     }
