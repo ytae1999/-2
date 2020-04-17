b3dd749270168bab08ce7ff66f626924d494bba4
태영준 <ytae1999@naver.com>
2020-04-17 11:51:54 +0900

    merge

--- Robot.java
+++ Robot.java
@@ -10,33 +10,14 @@ public class Robot
     int speed; //변수 정의
 
     public static int RobotWalk(String trafficLight, int speed){                       
-<<<<<<< HEAD
-        if(trafficLight == "green"){
-            if(trafficLight == "yellow"){
-                speed = 1.2 * speed;
-        } else {
-            
-        ..
-            
-=======
         if(trafficLight != "green"){            
-<<<<<<< HEAD
             if(trafficLight == "yellow" || trafficLight != "yellow"){  
                 speed = (int)(speed*1.2);
                 if(trafficLight == "red"){
                     speed = 0;                   
                 }                 
             }          
-=======
-            if(trafficLight == "yellow"){ 
-                speed = (int)(speed*1.2);                            
-            }
-            if(trafficLight == "red"){
-                speed = 0;                      
-                }     
->>>>>>> dc0925b81bec1f0320732a1911d38e23a5d60265
         }         
         return speed;
->>>>>>> f7a121f3878a4479cd4a878a75c8752048476742
     }
 }
