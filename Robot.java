62bcaf17d0aca1490aa2bdfb4a06c89e07250fb2
태영준 <ytae1999@naver.com>
2020-04-22 23:08:43 +0900

    Robot 클래스 수정과 Unit Test 기능을 이용한 RobotTest 생성 및 사용

--- Robot.java
+++ Robot.java
@@ -2,27 +2,26 @@
  * RobotWalk 메소드에 대한 코드
  * 
  * @author (2018315005 유제훈, 2018315033 태영준)
- * @version (2020.04.10) 
+ * @version (2020.04.23) 
  */
 public class Robot
 {           
-    String trafficLight; //변수 정의
     double speed; //변수 정의
-    
+
+    public void setSpeed(double speed){
+        this.speed = speed;
+    }
+
     public double getSpeed(){
         return speed;
     }
-    
-    public void setSpeed(double Speed){
-        this.speed = speed;
-    }
-    
-    public void RobotWalk(String trafficLight, double speed){                       
+
+    public void RobotWalk(String trafficLight){                       
         if(trafficLight == "green"){            
         }else if(trafficLight == "yellow"){
-            speed = speed * 1.2;           
+            speed = speed * 1.2;                
         }else{
             speed = 0;
-            }       
+        }
     }
 }
--- RobotTest.java
+++ RobotTest.java
@@ -1,17 +1,60 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
- * Robot클래스를 실행시키는 메인 클래스
+ * The test class RobotTest.
  *
- * @author (2018315005 유제훈, 2018315033 태영준)
- * @version (2020.04.10)
+ * @author  (your name)
+ * @version (a version number or a date)
  */
-public class RobotTest extends Robot
+public class RobotTest
 {
-    public static void main(String[] args){               
-        System.out.println("교통신호가 녹색이므로 " + RobotWalk("green", 5) +
-                            "km/h" + " 속도로 보행 계속");
-        System.out.println("교통신호가 노란색이므로 " + RobotWalk("yellow", 5) + 
-                            "km/h" + " 속도로 보행 계속");
-        System.out.println("교통신호가 빨간색이므로 " + RobotWalk("red", 5) + 
-                            "km/h" + " 속도로 보행 정지");
+    /**
+     * Default constructor for test class RobotTest
+     */
+    public RobotTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+
+    @Test
+    public void RobotWalk_Test()
+    {
+        Robot robot1 = new Robot();
+        robot1.setSpeed(5.0);
+        robot1.RobotWalk("green");
+        assertEquals(5.0, robot1.getSpeed(), 0.1);
+        Robot robot2 = new Robot();
+        robot2.setSpeed(5.0);
+        robot2.RobotWalk("yellow");
+        assertEquals(6.0, robot2.getSpeed(), 0.1);
+        Robot robot3 = new Robot();
+        robot3.setSpeed(5.0);
+        robot3.RobotWalk("red");
+        assertEquals(0.0, robot3.getSpeed(), 0.1);
     }
 }
+
